793858a86ab6f2d1f103cea417498776ea43beeb
Dilantha Wijesinghe <wdilantha66@gmail.com>
2023-11-28 01:30:22 +0530

    Refactor NavItems component to use PRODUCT_CATEGORIES and useRef

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,13 +1,44 @@
 "use client";
 
-import { useState } from "react";
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useState, useRef } from "react";
+import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+  const isAnyOpen = activeIndex !== null;
+
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
     <>
-      <div className="flex gap-4 h-full"></div>
+      <div className="flex gap-4 h-full" ref={navRef}>
+        {PRODUCT_CATEGORIES.map((category, i) => {
+          const handleOpen = () => {
+            if (activeIndex === i) {
+              setActiveIndex(null);
+            } else {
+              setActiveIndex(i);
+            }
+          };
+
+          const isOpen = i === activeIndex;
+
+          return (
+            <NavItem
+              category={category}
+              handleOpen={handleOpen}
+              isOpen={isOpen}
+              key={category.value}
+              isAnyOpen={isAnyOpen}
+            />
+          );
+        })}
+      </div>
     </>
   );
 };
