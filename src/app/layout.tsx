8c37a2bfe54b396f7a49bc1c84ed7bbc1647af35
Dilantha Wijesinghe <wdilantha66@gmail.com>
2023-11-27 19:43:05 +0530

    Refactor layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,10 +22,8 @@ export default function RootLayout({
         className={cn("relative h-full font-sans antialiased", inter.className)}
       >
         <main className="relative flex flex-col min-h-screen">
-          <div className="flex-grow flex-1">
-            <Navbar />
-            {children}
-          </div>
+          <Navbar />
+          <div className="flex-grow flex-1">{children}</div>
         </main>
       </body>
     </html>
