8d7b8892777aed52c2b31698e845125193d6ef32
Dilantha Wijesinghe <wdilantha66@gmail.com>
2023-11-27 14:10:56 +0530

    Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper>
-      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl"></div>
-    </MaxWidthWrapper>
+    <>
+      <MaxWidthWrapper>
+        <div className="flex flex-col items-center max-w-3xl py-20 mx-auto text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Your marketplace for high-quality{" "}
+            <span className="text-blue-600">digital assets</span>
+          </h1>
+          <p className="mt-6 text-lg text-muted-foreground max-w-prose">
+            Welcome to DigitalHippo. Every asset on our platform is verified by
+            our team to ensure our highest quality standards.
+          </p>
+          <div className="flex flex-col gap-4 mt-6 sm:flex-row">
+            <Link href="/products" className={buttonVariants()}>
+              Browse Trending
+            </Link>
+            <Button variant="ghost">Our quality promise &rarr;</Button>
+          </div>
+        </div>
+
+        {/* TODO: List Products */}
+      </MaxWidthWrapper>
+
+      <section>
+        <MaxWidthWrapper>
+          <div></div>
+        </MaxWidthWrapper>
+      </section>
+    </>
   );
 }
